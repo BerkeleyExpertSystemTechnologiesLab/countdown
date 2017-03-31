ea3a1e14767ed61fbc8bf485ee1f6dd800cc42fb
Douglas Hutchings <douglashutchings@pioneers.berkeley.edu>
2017-03-31 03:43:13 +0100

    First attempt at pulling master on reboot.

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 #/!/bin/bash
 
+sleep 60 #wait for the computer to turn on
+cd ~/countdown
+git pull
+sleep 30 #wait for git pull to work
 epiphany-browser  ~/countdown/count.html &
 sleep 15
 xte "key F11" -x:0
