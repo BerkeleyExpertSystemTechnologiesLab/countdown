1dce30ab3bbb23a64c7957bc1f44c857e33864d2
Douglas Hutchings <doug@squishy-robotics.com>
2018-10-08 23:26:49 -0700

    Now lage the countdown folder on the desktop

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #/!/bin/bash
 
 sleep 60 #wait for the computer to turn on
-cd ~/countdown
+cd ~/Desktop/countdown
 git pull
 sleep 30 #wait for git pull to work
-epiphany-browser  ~/countdown/count.html &
+epiphany-browser  ~/Desktop/countdown/count.html &
 sleep 15
 xte "key F11" -x:0
