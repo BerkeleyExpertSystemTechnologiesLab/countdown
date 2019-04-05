a25c69322deaa301584f3e524266e00ef6fa4b7a
Douglas Hutchings <doug@squishy-robotics.com>
2019-04-04 23:06:10 -0700

    hack to fix fuzzy numbers

--- script.sh
+++ script.sh
@@ -7,3 +7,13 @@ sleep 30 #wait for git pull to work
 epiphany-browser  ~/Desktop/countdown/count.html &
 sleep 15
 xte "key F11" -x:0
+sleep 15
+
+#a hack - zoom in and out again.  this will force the screen to change from fuzzy numbers to crisp numbers.  dont' know why.  zoom in and out to keep position the same
+
+xte "keydown Control_L" "key equal" "keyup Control_L" -x:0
+sleep 15
+xte "keydown Control_L" "key minus" "keyup Control_L" -x:0
+
+
+
